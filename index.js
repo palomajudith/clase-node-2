381bdaf3072596917e94a0c4b99e14677ace051f
palomajudith <judith_ortega94@outlook.com>
2020-10-24 11:30:20 -0600

    formasdefiguras

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("ejercicio con node y expres");
+
+//  index.js
+const { triángulo , cuadrado , rombo } = require ( 'forma de impresión ' )       
+ 
+triángulo ( 6 )
+cuadrado ( 6 )
+rombo ( 6 )
